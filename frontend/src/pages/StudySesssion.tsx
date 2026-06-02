31f7cd5621697ec728dead4732dcaabe68990bf1
kenta kawasaki <tomidakennta@icloud.com>
2026-06-02 11:55:57 +0900

    WIP: navigationに紐づくpageの作成

---
+++ frontend/src/pages/StudySesssion.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../components/Header"
+
+export const StudySession = () => {
+    return (
+        <>
+            <Header />
+            <h1>語彙管理ページです</h1>
+        </>
+    )
+}
