58da5c8bda1821d5b48f3f1f8558cf78c4ef294c
shouta0715 <kshouta0715@gmail.com>
2026-06-13 16:57:04 +0900

    feat: スコアチャートの完成

--- frontend/src/components/home/ScoreChart.tsx
+++ frontend/src/components/home/ScoreChart.tsx
@@ -1,7 +1,10 @@
+import { Chart } from "../../modules/Chart";
+
 export const ScoreChart = () => {
-    return (
-        <div>
-            スコアチャート
-        </div>
-    )
-}
+  return (
+    <div className="bg-white rounded-xl p-4 border border-gray-300 max-w-[550px]">
+      <p className="mb-3 text-xl font-medium text-gray-600">スコア推移</p>
+      <Chart />
+    </div>
+  );
+};
