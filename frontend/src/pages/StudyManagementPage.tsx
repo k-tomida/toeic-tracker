8c4548aabe70ba177e174bbe93c5eeec4b38947f
kenta kawasaki <tomidakennta@icloud.com>
2026-06-19 12:51:31 +0900

    feat: 学習記録ページの土台を作成

--- frontend/src/pages/StudyManagementPage.tsx
+++ frontend/src/pages/StudyManagementPage.tsx
@@ -1,10 +1,21 @@
 import { Header } from "../components/Header"
+import { Button } from "../ui/Button"
 
 export const StudyManagementPage = () => {
     return (
-        <>
+        <div className="min-h-screen ">
             <Header />
-            <h1>学習記録ページです</h1>
-        </>
+            <main className="max-w-7xl mx-auto px-4 py-6">
+                <div className="flex justify-between">
+                    <h1 className="text-xl ml-6">学習記録</h1>
+                    <Button>
+                        <div className="flex items-center gap-3">
+                            <span>+</span>
+                            <span>セッションを追加</span>
+                        </div>
+                    </Button>
+                </div>
+            </main>
+        </div>
     )
 }
