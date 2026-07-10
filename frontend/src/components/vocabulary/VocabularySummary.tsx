c0e44fe7e08a047756eaa7280dd902f4cdfef7d4
kenta <tomidakennta@icloud.com>
2026-07-11 00:57:48 +0900

    WIP: ダミーの語彙データとtypeの追加

---
+++ frontend/src/components/vocabulary/VocabularySummary.tsx
@@ -0,0 +1,15 @@
+export const VocabularySummary = () => {
+    return (
+        <div className="bg-white rounded-xl p-4 border border-gray-300 flex-1 min-w-[400px]">
+            <h2 className="mb-3 text-xl font-medium text-gray-600"> 語彙サマリー</h2>
+            <div>
+                <div className="flex mx-auto justify-center flex-wrap xl:gap-8">
+
+                </div>
+                <div className="flex mx-auto justify-center flex-wrap xl:gap-8">
+
+                </div>
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/VocabularyPage.tsx
+++ frontend/src/pages/VocabularyPage.tsx
@@ -1,10 +1,15 @@
 import { Header } from "../components/Header"
+import { VocabularySummary } from "../components/vocabulary/VocabularySummary"
 
 export const VocabularyPage = () => {
     return (
-        <>
+        <div className="min-h-screen">
             <Header />
-            <h1>語彙管理ページです</h1>
-        </>
+            <main className="max-w-7xl mx-auto px-4 py-6">
+                <div className="flex flex-wrap gap-4 mx-10 my-5">
+                    <VocabularySummary />
+                </div>
+            </main>
+        </div>
     )
 }
