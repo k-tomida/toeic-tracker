5f2950ad8b5fb5c51843e9203e7df1f762881c1e
kenta <tomidakennta@icloud.com>
2026-07-19 22:15:40 +0900

    feat: flywayで仮のuserデータ追加とUserエンティティの追加

---
+++ backend/src/main/resources/db/migration/V2__insert_sample_user.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (
+    email,
+    password,
+    name,
+    target_score,
+    next_exam_date
+)
+VALUES (
+           'test@example.com',
+           '$passwrd',
+           'Kenta',
+           900,
+           '2026-12-25'
+       );
