a601512116ff3fbb9b6beb6651d1f191723a54b9
kenta <tomidakennta@icloud.com>
2026-08-17 00:25:22 +0900

    test: フロントエンドの学習時間計算の関数にテストを実装

--- frontend/src/utils/calcStudyTime.ts
+++ frontend/src/utils/calcStudyTime.ts
@@ -18,7 +18,7 @@ export const calcStudyTimeInWeek = (day: Date, data: studySessionType[]): number
         }
     });
 
-    return Math.round(count / 60);
+    return Math.round(count / 60 * 10) / 10;
 };
 
 export const calcStudyTimeInMonth = (day: Date, data: studySessionType[]): number => {
@@ -32,7 +32,7 @@ export const calcStudyTimeInMonth = (day: Date, data: studySessionType[]): numbe
             count += d.duration;
         }
     })
-    return Math.round(count / 60);
+    return Math.round(count / 60 * 10) / 10;
 
 }
 
@@ -42,7 +42,7 @@ export const calcStudyTimeAll = (data: studySessionType[]): number => {
         count += d.duration;
     })
 
-    return Math.round(count / 60);
+    return Math.round(count / 60 * 10) / 10;
 }
 
 export const calcStudyTimeAverage = (data: studySessionType[]): number => {
@@ -73,6 +73,6 @@ export const calcStudyTimeByCategory = (category: categoryType, data: studySessi
             }
         }
     })
-    return Math.round(count / 60);
+    return Math.round(count / 60 * 10) / 10;
 }
 
